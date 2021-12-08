a6a27eccd5dd49816bec799b629898bc3a8380f9
Tommy Alpert <tommy0356@gmail.com>
2021-12-08 18:30:58 -0500

    1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
 
   return (
     <>
-      <Header course={course}/>
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name}/>
+      <Content parts={[course.parts[0].name, course.parts[1].name, course.parts[2].name]} exercises={[course.parts[0].exercises, course.parts[1].exercises, course.parts[2].exercises]} />
+      <Total exercises={[course.parts[0].exercises, course.parts[1].exercises, course.parts[2].exercises]} />
     </>
   )
 }
